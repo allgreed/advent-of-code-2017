ae01dc86535f97cdda61c66b7093220e50c189f4
allgreed <olixem@gmail.com>
2017-12-05 01:31:11 +0100

    Further improved :D

--- 4/second/main.js
+++ 4/second/main.js
@@ -5,34 +5,12 @@ const data = require("fs").readFileSync("../input.txt", 'utf-8')
     .slice(0, -1)
     .map(line => line.split(" "))
 
-const isAnagramOf = (firstWord, secondWord) =>
-{
-    // todo: refactor this like shit
-    firstWord = firstWord.split("")
-    secondWord = secondWord.split("")
-
-    if (firstWord.length !== secondWord.length)
-        return false
-
-    const removeFrom = arr => value =>
-    {
-        if (arr.includes(value))
-            arr.splice(arr.indexOf(value), 1)
-    }
-
-    const removeOnce = removeFrom(firstWord)
-
-    for (char of secondWord)
-        removeOnce(char)
-
-    return firstWord.length === 0 
-
-}
+const isAnagram = require('anagram-checker')
 
 const isLineValid = arr =>
 {
-    for (const comparedWord = arr.shift(); comparedWord !== undefined; comparedWord = arr.shift())
-        if (!arr.every(element => !(isAnagramOf(element, comparedWord))))
+    for (let comparedWord = arr.shift(); comparedWord !== undefined; comparedWord = arr.shift())
+        if (!arr.every(element => !(isAnagram(element, comparedWord))))
             return false
 
     return true
