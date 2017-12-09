f0ae76cf368e106313d80017bb9bb2da7415265d
allgreed <olixem@gmail.com>
2017-12-09 13:53:26 +0100

    Refactored second part

--- 9/second/main.js
+++ 9/second/main.js
@@ -1,26 +1,12 @@
 #!/usr/bin/env node
 
 const filename = "input"
-const data = require("fs").readFileSync(`../${ filename }.txt`, 'utf-8')
-    .slice(0, -1)
-    .split("")
-
-//todo: refactor to reduce with passing state object between function calls
-let sum = 0, skip_flag = false
-
-for (let i = 0; i < data.length; ++i)
-{
-    character = data[i]
-    
-    if (character == '!')
-        ++i
-    else if (skip_flag && character != '>')
-        ++sum 
-    else if (character == '<')
-        skip_flag = true
-    else if (character == '>')
-        skip_flag = false 
-}
+const sum = require("fs").readFileSync(`../${ filename }.txt`, 'utf-8')
+    .slice(0, -1) // trailing whitespace
+    .replace(/\!./g, "") // removing bangs 
+    .match(/<([^>]*)>/g).map(match => match.replace(/<([^>]*)>/g, '$1')) // extracting stuff between '<' and '>'
+    .map(captureGroup => captureGroup.length) // counting characters
+    .reduce((a,b) => a +b) // suming it all up
 
 console.log(sum)
 
